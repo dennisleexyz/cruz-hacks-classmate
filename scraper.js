b972d5dc854acbde3781bcc906739a1a32017ccd
Dennis Lee <51690327+dennisleexyz@users.noreply.github.com>
2020-01-18 09:15:01 -0800

    feat: make scraper.js operate on entire course list

--- scraper.js
+++ scraper.js
@@ -2,6 +2,17 @@
 const fetch = require('node-fetch')
 const parse5 = require('parse5');
 let table = []
+let css = [
+	"desc",
+	"sc-credithours",
+	"instructor",
+	"extraFields",
+	"genEd",
+	"quarter",
+]
+function parseTag(tag) {
+	throw 'notimplemented'
+}
 const traverse = require('parse5-traverse');(async () => {
   try {
     const data = await fetch('https://catalog.ucsc.edu/Current/General-Catalog/Courses/AM-Applied-Mathematics', {
@@ -10,21 +21,26 @@ const traverse = require('parse5-traverse');(async () => {
     const stuff = await data.text()
     // console.log('done fetching')
     const document = parse5.parse(stuff);
+    // We should grab just the .courselist and use that as the scope
     traverse(document, {
       pre(node, parent) {
         // console.log('faaa',node)
         if (node.nodeName === 'div') {
+	  console.dir(node.attrs)
           // console.log('eee', node)
-          node.attrs.forEach(attr => {
-            if (attr.name === 'class' && attr.value === 'desc') {
-              node.childNodes.forEach(childNode => {
-                // console.info(childNode.nodeName, childNode.value)
-                console.info(node.nodeName, node.value)
-		table.push({ [childNode.nodeName] : childNode.value })
-              })
-              // console.log('33333', node)
-            }
-          })
+	  className = 'desc'
+          //css.forEach(className => {
+            node.attrs.forEach(attr => {
+              if (attr.name === 'class' && attr.value === className) {
+                node.childNodes.forEach(childNode => {
+                  // console.info(childNode.nodeName, childNode.value)
+                  //console.info(node.nodeName, node.value)
+  	          table.push({ [childNode.nodeName] : childNode.value })
+                })
+                // console.log('33333', node)
+              }
+            })
+          //})
         }
         // node -> the current node
         // parent -> the parent node
