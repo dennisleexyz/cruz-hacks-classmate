5004e8ca928ae3d5ae1a55cc367c0cb2dcc86396
Dennis Lee <51690327+dennisleexyz@users.noreply.github.com>
2020-01-18 22:40:40 -0800

    feat: add course lvl col to table

--- client/src/components/CourseTable.vue
+++ client/src/components/CourseTable.vue
@@ -16,13 +16,14 @@
             <table class="table table-striped table-hover">
               <thead>
                 <tr>
-                 <th>Department</th>
-                <th>Course #</th>
-                <th>Course Title</th>
-                <th>Units</th>
-                <th>Instructor</th>
-                <th>Requirements</th>
-                <th>General Education Code</th>
+                  <th>Department</th>
+                  <th>Course #</th>
+                  <th>Course Title</th>
+                  <th>Course Level</th>
+                  <th>Units</th>
+                  <th>Instructor</th>
+                  <th>Requirements</th>
+                  <th>General Education Code</th>
                 </tr>
               </thead>
               <tbody>
@@ -35,6 +36,7 @@
                     <td><a :href="foo.link" :title="Object.entries(course)[0][0].replace(/-/g, ' ').replace(' ', ' - ')" target="__blank">{{ foo.departmentName }}</a></td>
                 <td><a :href="foo.link" :title="foo.shortName" target="__blank">{{ foo.courseNumber }}</a></td>
                 <td><span :title=foo.descriptionTest>{{ foo.longName }}</span></td>
+                <td>{{ courseLevel(foo.courseNumber) }}</td>
                 <td>{{ foo.credits }}</td>
                 <td>{{ foo.professors.toString().replace(/,/g, ', ') }}</td>
                 <td>{{ foo.prereqs }}</td>
@@ -139,6 +141,18 @@ export default {
         console.log(objs);
         return foo[this.currentQuarter];
       });
+    },
+    courseLevel(code) {
+      let num = code.replace(/[A-Z]*/g, '')
+      if(num < 100) {
+	return("Lower Division")
+      }
+      else if(num < 200) {
+	return("Upper Division")
+      }
+      else {
+	return("Graduate")
+      }
     }
   }
 };
